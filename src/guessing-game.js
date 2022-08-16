c51ea78d4f3e7e0d73342be485d5daebb191f1d9
Milsevka <lusindami99@gmail.com>
2022-08-16 19:32:40 +0300

    feat game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.max = max - 1
+        this.min = min + 1
     }
 
     guess() {
-
+return Math.round(this.min + ((this.max - this.min) / 2))
     }
 
     lower() {
-
+this.max = this.guess() - 1
     }
 
     greater() {
-
+        this.min = this.guess() + 1
     }
 }
 
